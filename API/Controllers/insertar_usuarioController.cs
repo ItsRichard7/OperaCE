c2c9bcdea19f9bafbe3b64d1412ea512f57e4de3
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2024-04-24 14:36:25 -0600

    API Base

---
+++ API/Controllers/insertar_usuarioController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("insertar_usuario")]
+
+    public class insertar_usuarioController : ControllerBase
+    {
+        public dynamic insertar_usuario()
+        {
+            return "insertar_usuario";
+        }
+    }
+}
