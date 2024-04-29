9eb631010112dc067f2a9b4e4e8f0e94ee274d45
ItsRichard7 <ricardobm@estudiantec.cr>
2024-04-29 17:30:01 -0600

    Finalizar SP de Vista Profesor

--- Scripts/SpProfe.sql
+++ Scripts/SpProfe.sql
@@ -25,6 +25,57 @@ BEGIN
 END
 GO
 
+-- Rechazar un prestamo de activo y eliminarlo (Vista Aprobar Prestamos)
+CREATE PROCEDURE rechazar_prestamo_activo (@correo_soli NVARCHAR(50), @fecha_soli DATE, @hora_soli TIME)
+AS
+BEGIN
+	DELETE FROM Soli_Act WHERE correo_soli = @correo_soli AND fecha_soli = @fecha_soli AND hora_soli = @hora_soli;
+END
+GO
+
+-- Obtener Lista de Labs (este ya esta en SpAdmin con el nombre de obt_laboratorios)
+
+-- Verificar que no haya otra solicitud del laboratorio (Vista Reservación Laboratorios)
+CREATE PROCEDURE hay_choque_reservas (@lab_nombre NCHAR(6), @hora_inicio TIME, @cant_horas DECIMAL(2,1), @existe_registro BIT OUTPUT)
+AS
+BEGIN
+	DECLARE @hora_salida TIME
+	SET @hora_salida = DATEADD(HOUR, @cant_horas, @hora_inicio)
+	IF EXISTS ( SELECT 1 FROM Soli_Lab
+        WHERE lab_nombre = @lab_nombre AND hora >= @hora_inicio AND hora <= @hora_salida
+    )
+    BEGIN SET @existe_registro = 1 END
+    ELSE BEGIN SET @existe_registro = 0 END
+END
+GO
+
+-- Insertar una nueva solicitud de laboratorio (Vista Reservación Laboratorios)
+CREATE PROCEDURE insertar_soli_lab (@correoSoli NVARCHAR(50), @fechaSoli DATE, @horaSoli TIME, @carnet NUMERIC(12), 
+                                    @pNombre NVARCHAR(20), @sNombre NVARCHAR(20), @pApellido NVARCHAR(20), @sApellido NVARCHAR(20), 
+									@cantHoras DECIMAL(2,1), @labNombre NCHAR(6), @userCed NUMERIC(10))
+AS
+BEGIN
+    INSERT INTO Soli_Lab (correo_soli, fecha, hora, carnet, p_nombre, s_nombre, p_apellido, s_apellido, cant_horas, lab_nombre, user_ced)
+    VALUES (@correoSoli, @fechaSoli, @horaSoli, @carnet, @pNombre, @sNombre, @pApellido, @sApellido, @cantHoras, @labNombre, @userCed)
+END
+GO
+
+-- Obtener todas las solicitudes que ha tenido un laboratorio (Vista Calendario Laboratorio)
+CREATE PROCEDURE obt_solis_lab (@nombre NCHAR(6))
+AS
+BEGIN
+    SELECT * FROM Soli_Lab WHERE lab_nombre = @nombre;
+END
+GO
+
+-- Obtener todas las solicitudes de laboratorios que ha hecho un profesor (Vista Registro Reservaciones)
+CREATE PROCEDURE obt_solis_labs_profe (@cedula NUMERIC(10))
+AS
+BEGIN
+	SELECT lab_nombre, fecha, hora, cant_horas FROM Soli_Lab WHERE user_ced = @cedula;
+END
+GO
+
 /* Eliminar todos los store procedure
 DROP PROCEDURE obt_activos_no_aprobados;
 DROP PROCEDURE aprobar_prestamo_activo;
@@ -34,3 +85,4 @@ DROP PROCEDURE aprobar_prestamo_activo;
 SELECT * from Sys.procedures; --Ignorar este comando
 EXEC obt_activos_no_aprobados 3456789012;
 EXEC obt_activos_no_aprobados 'juanitoelcrack@estudiantec.cr', '2024-04-28', '18:56:44';
+*/
