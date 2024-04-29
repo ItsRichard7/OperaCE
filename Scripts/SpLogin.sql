361066644e5bac54d63398d4c171fc270733b009
ItsRichard7 <ricardobm@estudiantec.cr>
2024-04-29 16:31:10 -0600

    Adaptar insertar usuario a operadores y profes

--- Scripts/SpLogin.sql
+++ Scripts/SpLogin.sql
@@ -19,8 +19,12 @@ GO
 CREATE PROCEDURE insertar_usuario (@cedula NUMERIC(10),@correo NVARCHAR(50),@contrasena NVARCHAR(20), @carnet NUMERIC(12), @p_nombre NVARCHAR(20), @s_nombre NVARCHAR(20), @p_apellido NVARCHAR(20), @s_apellido NVARCHAR(20), @f_nacim DATE, @rol_id NUMERIC(1))
 AS
 BEGIN
+	DECLARE @activo BIT;
+	IF @rol_id = 2 SET @activo = 1;
+	ELSE SET @activo = 0;
+
     INSERT INTO Usuario (cedula, correo, contrasena, carnet, p_nombre, s_nombre, p_apellido, s_apellido, f_nacim, activo, rol_id)
-	VALUES (@cedula, @correo, @contrasena, @carnet, @p_nombre, @s_nombre, @p_apellido, @s_apellido, @f_nacim, 0, @rol_id);
+	VALUES (@cedula, @correo, @contrasena, @carnet, @p_nombre, @s_nombre, @p_apellido, @s_apellido, @f_nacim, @activo, @rol_id);
 END
 GO
 
